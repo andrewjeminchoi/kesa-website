a10ba83dd974c29fbd3719d4c2b790d8ded19ae2
Andrew Choi <andrewchoi96@hotmail.com>
2017-06-02 20:52:35 -0400

    Initial commit for website files

---
+++ js/navBar.js
@@ -0,0 +1,11 @@
+/* JS script for a responsive top naviagtion bar */
+/* UTKESA 2017-2018 */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
